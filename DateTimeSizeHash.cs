787b3848f562501c0d44b11b66747d550083c18e
sbs20 <sam.strachan@gmail.com>
2016-05-07 16:18:24 +0100

    Minor improvement to DateTimeSizeHash

--- DateTimeSizeHash.cs
+++ DateTimeSizeHash.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using System.IO;
-using System.Text;
 
 namespace Sbs20.Syncotron
 {
-    class DateTimeSizeHash : IHashProvider
+    public class DateTimeSizeHash : IHashProvider
     {
         public string Hash(FileInfo fileInfo)
         {
-            string s = fileInfo.LastWriteTimeUtc.ToString("o") + ":" + fileInfo.Length;
-            byte[] b = Encoding.UTF8.GetBytes(s);
+            byte[] b = new byte[16];
+            Array.Copy(BitConverter.GetBytes(fileInfo.LastWriteTimeUtc.Ticks), b, 8);
+            Array.Copy(BitConverter.GetBytes(fileInfo.Length), 0, b, 8, 8);
             return Convert.ToBase64String(b);
         }
     }
