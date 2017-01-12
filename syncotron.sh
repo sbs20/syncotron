9423dc9097987f35fb681d3fb0aacfebac7fe2a3
sbs20 <sam.strachan@gmail.com>
2017-01-12 12:53:27 +0000

    Updated syncotron.sh to run only one instance

--- syncotron.sh
+++ syncotron.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 export MONO_IOMAP=all
-mono /opt/syncotron/bin/syncotron.exe "$@"
+cmd="mono /opt/syncotron/bin/syncotron.exe $@"
+
+# List all processes | only those with these arguments (with fixed string -F because of "*") | but exclude the grep
+ps_out=$(ps -ef | grep -F -- "${cmd}" | grep -v 'grep')
+
+result=$(echo $ps_out)
+
+# If there is a result then we're still running
+if [[ "$result" != "" ]];then
+    echo "This syncotron is already running"
+    echo "($result)"
+else
+    echo "Starting..."
+    eval $cmd
+fi
